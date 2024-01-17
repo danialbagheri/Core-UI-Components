a1e1a6c84e3dc493a311d61217df1de2004fbcbd
e.khaleghian <e.khaleghian@rooberah.co>
2024-01-17 18:27:57 +0330

    feat: Product item change

--- products/productRange/ProductRangeItem.jsx
+++ products/productRange/ProductRangeItem.jsx
@@ -18,6 +18,8 @@ import {ShowPrice} from 'sharedComponents'
 export default function ProductRangeItem(props) {
   const {product} = props
 
+  console.log('🚀 🙂  product:::', product)
+
   const router = useRouter()
   const [activeVariant, setActiveVariant] = React.useState(product.variants[0])
 
@@ -52,21 +54,48 @@ export default function ProductRangeItem(props) {
           router.push(`/products/${product.slug}`)
         }}
         sx={{
+          height: 369,
+          background:
+            'linear-gradient(180deg, #FAF5EB 0%, rgba(250, 245, 235, 0.00) 100%)',
+
+          position: 'relative',
+
+          pt: '26px',
+          // pb: '20px',
           display: 'flex',
           flexDirection: 'column',
-          gap: 3,
+          justifyContent: 'space-between',
           cursor: 'pointer',
         }}
       >
+        {product.collection_names.length > 0 ? (
+          <Box
+            sx={{
+              borderRadius: '3px',
+              fontSize: '11px',
+              fontWeight: 700,
+              zIndex: 2,
+              position: 'absolute',
+              top: 10,
+              right: 10,
+              color: 'black',
+              backgroundColor: '#fff',
+              padding: '3px 10px',
+            }}
+          >
+            {product.collection_names[0]}
+          </Box>
+        ) : null}
         <Box
           sx={{
-            background:
-              'linear-gradient(180deg, rgba(245,245,247,1) 100%, rgba(255,255,255,1) 100%)',
             width: '100%',
             height: '300px',
+            maxHeight: '300px',
+            minHeight: '300px',
             margin: '0 auto',
             position: 'relative',
             transition: 'all 200ms',
+            bgcolor: 'red',
             borderRadius: 1,
             '&:hover': {
               background:
@@ -74,27 +103,9 @@ export default function ProductRangeItem(props) {
             },
           }}
         >
-          {product.collection_names.length > 0 ? (
-            <Box
-              sx={{
-                borderRadius: '3px',
-                fontSize: '11px',
-                fontWeight: 700,
-                zIndex: 2,
-                position: 'absolute',
-                top: 10,
-                right: 10,
-                color: 'black',
-                backgroundColor: '#fff',
-                padding: '3px 10px',
-              }}
-            >
-              {product.collection_names[0]}
-            </Box>
-          ) : null}
           <Image
             alt={product.name}
-            fill={true}
+            fill
             src={imageSrcHandler(
               activeVariant.image_list[0],
               product.main_image,
@@ -102,12 +113,19 @@ export default function ProductRangeItem(props) {
             style={{objectFit: 'contain'}}
           />
         </Box>
-        <Typography variant="h5">{product.name}</Typography>
-        <Typography>{product.sub_title}</Typography>
+        <Box className="centralize" gap="10px">
+          <StarRating
+            name={product.name}
+            score={product.review_average_score}
+          />
+          <Typography color="#333">{product.total_review_count}</Typography>
+        </Box>
       </Box>
 
       <Box sx={{display: 'flex', flexDirection: 'column', gap: 3}}>
-        <StarRating name={product.name} score={product.review_average_score} />
+        <Typography variant="h5">{product.name}</Typography>
+        <Typography>{product.sub_title}</Typography>
+
         <hr className="m-0" />
         <ShowPrice selectedVariant={activeVariant} />
         <AddToBasketWithDropDown
