dd8e82d2036f0dc3e7fbcc7c5b5b593526a0fe35
e.khaleghian <e.khaleghian@rooberah.co>
2024-03-13 19:44:53 +0330

    change aboute us intorodcution

--- about/Introduction.js
+++ about/Introduction.js
@@ -7,12 +7,15 @@ export default function Introduction() {
         We&apos;re Calypso, we have been protecting your skin since 1988
       </h3>
       <p className="text-centre introduction">
-        Calypso is a British sun care brand which aims to provide customers with
-        quality sun care products at exceptionally low prices. Calypso sun
-        protection is available to buy from Morrisons, Asda, Wilko, TK Maxx,
-        Matalan, Home Bargains, B&M Bargains, Bodycare, TJ Hughes, Well
-        Pharmacy, Semichem and independent pharmacies. Our products are not only
-        available to buy in the UK, but also many countries around the globe.
+        Born in rainy Manchester, Calypso brings the sunshine wherever you go!
+        We&apos;re not your average sun care brand. We&apos;re a family
+        favorite, built on a legacy of our unique, striking formula loved by
+        many. Our mission is to make sun safety accessible to everyone.
+        That&apos;s why we offer premium protection at unbelievably low prices.
+        Our complete range caters to all skin types and sun-loving needs.Join
+        the Calypso family for a lifetime of sun-safe adventures! Find us at
+        your favourite retailers like Morrisons, Asda, TK Maxx, and countless
+        stores worldwide. Let&apos;s chase the sunshine together.
       </p>
     </section>
   )
