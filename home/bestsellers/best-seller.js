21c71b9b7a11b71e591a8a314ed979a1630eb32d
e.khaleghian <e.khaleghian@rooberah.co>
2024-04-04 17:23:10 +0330

    fix: best-seller spaces

--- home/bestsellers/best-seller.js
+++ home/bestsellers/best-seller.js
@@ -44,11 +44,12 @@ export default function BestSeller() {
       sx={{
         maxWidth: '1600px',
         margin: '0 auto',
-        mt: 5,
+        mt: 10,
         mb: {xs: 10, sm: 20, my: 7},
+        p: 5,
       }}
     >
-      <Typography color="earth.main" sx={{textAlign: 'center'}} variant="h3">
+      <Typography color="earth.main" sx={{textAlign: 'left'}} variant="h3">
         Top Seller products
       </Typography>
 
@@ -57,7 +58,7 @@ export default function BestSeller() {
           display: 'flex',
           gap: 5,
           flexDirection: {xs: 'column', lg: 'row'},
-          padding: 5,
+          mt: 5,
         }}
       >
         <Box
